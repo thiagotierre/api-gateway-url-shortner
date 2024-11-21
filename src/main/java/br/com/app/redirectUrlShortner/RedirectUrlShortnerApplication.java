94bdc7ea6240b7b8c8fd4f625305c9c790ddd559
Thiago Tierre <thiagotierre.lima@gmail.com>
2024-11-21 16:49:25 -0300

    upload do projeto API gateway

---
+++ src/main/java/br/com/app/redirectUrlShortner/RedirectUrlShortnerApplication.java
@@ -0,0 +1,13 @@
+package br.com.app.redirectUrlShortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectUrlShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectUrlShortnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/app/redirectUrlShortner/RedirectUrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app.redirectUrlShortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectUrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
