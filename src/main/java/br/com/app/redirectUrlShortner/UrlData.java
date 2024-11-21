c3306710edee48e92ef68184131a4c32eeaf1acb
Thiago Tierre <thiagotierre.lima@gmail.com>
2024-11-21 18:27:20 -0300

    Criado classe para mapear arquivo salvo no servico s3 da aws

---
+++ src/main/java/br/com/app/redirectUrlShortner/UrlData.java
@@ -0,0 +1,15 @@
+package br.com.app.redirectUrlShortner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+    private String originalURL;
+    private long expirationTime;
+}
